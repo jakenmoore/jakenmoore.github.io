2114ea22419cc5a757aef2544aeb62e808f2d615
Jake <jake.moore@mac.com>
2020-09-17 11:26:35 -0400

    rebuilding the site with new about page

--- content/about.md
+++ content/about.md
@@ -1,9 +1,7 @@
 ---
 title: "About"
 date: 2020-09-16T15:06:26-04:00
-draft: true
+draft: false
 ---
 
-This is about my blog. Basically it is whatever crap I can think of.
-
-Let's see if we can get this thing working. 
+This is the blog of Jake Moore. 
--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 
-# If a command fails then the deploy stops
-set -e
-
-printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
-
 # Build the project.
-hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+hugo -t cactus
 
 # Go To Public folder
 cd public
@@ -15,11 +10,7 @@ cd public
 git add .
 
 # Commit changes.
-msg="rebuilding site $(date)"
-if [ -n "$*" ]; then
-	msg="$*"
-fi
-git commit -m "$msg"
+git commit -m "rebuilding the site"
 
 # Push source and build repos.
 git push origin master
